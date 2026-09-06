4049f35f0a0d2cc12a1c278400b11da94f31b201
SubhasishKabi <99902592+SubhasishKabi@users.noreply.github.com>
2026-09-06 16:06:31 +0530

    s3 url

--- .github/actions/javascript-s3-deploy/main.js
+++ .github/actions/javascript-s3-deploy/main.js
@@ -11,7 +11,10 @@ function run() {
 
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-  core.notice("hello from custom js actions");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazon.aws.com`
+  core.setOutput('website-url', websiteUrl)
+
+  // core.notice("hello from custom js actions");
 }
 
 run()
